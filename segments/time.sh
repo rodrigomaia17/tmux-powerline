c40b0c14fad3544fc73c539a3b120724038c312f
Rodrigo Maia <rodrigo.maia.pereira@gmail.com>
2017-04-25 14:56:10 -0300

    adds toronto time

--- segments/time.sh
+++ segments/time.sh
@@ -18,7 +18,7 @@ __process_settings() {
 
 run_segment() {
         __process_settings
-        echo "Belo Horizonte" $(TZ=America/Sao_Paulo date +"$TMUX_POWERLINE_SEG_TIME_FORMAT")
+        echo "Toronto" $(TZ=America/Toronto date +"$TMUX_POWERLINE_SEG_TIME_FORMAT") "∞ Belo Horizonte" $(TZ=America/Sao_Paulo date +"$TMUX_POWERLINE_SEG_TIME_FORMAT")
         
 	return 0
 }
