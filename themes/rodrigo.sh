e7e06f8c99838e8d57a7cca14a52186615ca081f
Rodrigo Maia <rodrigo.maia.pereira@gmail.com>
2019-09-04 00:00:32 -0300

    :coffee:

---
+++ themes/rodrigo.sh
@@ -0,0 +1,63 @@
+# Default Theme
+
+if patched_font_in_use; then
+	TMUX_POWERLINE_SEPARATOR_LEFT_BOLD=""
+	TMUX_POWERLINE_SEPARATOR_LEFT_THIN=""
+	TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD=""
+	TMUX_POWERLINE_SEPARATOR_RIGHT_THIN=""
+else
+	TMUX_POWERLINE_SEPARATOR_LEFT_BOLD="◀"
+	TMUX_POWERLINE_SEPARATOR_LEFT_THIN="❮"
+	TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD="▶"
+	TMUX_POWERLINE_SEPARATOR_RIGHT_THIN="❯"
+fi
+
+TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR=${TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR:-'235'}
+TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR=${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR:-'255'}
+
+TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR:-$TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD}
+TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR:-$TMUX_POWERLINE_SEPARATOR_LEFT_BOLD}
+
+
+# Format: segment_name background_color foreground_color [non_default_separator]
+
+if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
+	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
+    #"tmux_session_info 148 234" \
+    "battery 55 255" \
+    #"hostname 33 0" \
+		#"ifstat 30 255" \
+		#"ifstat_sys 30 255" \
+    "disk_usage 55 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
+    "disk_usage_home 55 255" \
+    "uptime 61 255" \
+		#"wan_ip 32 255" \
+    #"vcs_branch 29 88" \
+		#"vcs_compare 60 255" \
+    #"rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}" \
+		#"vcs_staged 64 255" \
+		#"vcs_modified 9 255" \
+		#"vcs_others 245 0" \
+		"now_playing 32 255" \
+	)
+fi
+
+if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
+	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
+		#"earthquake 3 0" \
+		#"pwd 89 211" \
+		#"mailcount 9 255" \
+    #"cpu 240 136" \
+    "load 32 255" \
+    #"tmux_mem_cpu_load 234 255" \
+    #"battery 32 255" \
+    #"weather 37 255" \
+    #"rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}" \
+		#"xkb_layout 125 117" \
+		#"date_day 235 136" \
+		#"date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		"dollar 61 255" \
+		"time 55 255 ${TMUX_POWERLINE_SEPARATOR_LEFT_BOLD}" \
+		#"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+	)
+fi
